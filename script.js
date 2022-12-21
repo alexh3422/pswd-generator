890c58b8550c9f1c6c1598212cc51d037ba2b518
Alex Hall <alexhallinthecloud@icloud.com>
2022-12-20 19:36:30 -0800

    added functions and var to js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +11,57 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword () { 
+  var length = passwordlength();
+  var lowercase = passwordlowercase();
+  var uppercase = passworduppercase();
+  var numeric = passwordnumeric();
+  var specialcharacters = passwordspecialcharacters();
+  var lowercasecharacters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercasecharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericcharacters = "0123456789";
+  var specialcharacters = "!@#$%^&*;:<>?/|";
+}
+
+// add functionality to validate input - while loop 
+function passwordlength () {
+  var lengthisok = false
+  while (!lengthisok) {
+    var length = prompt("Enter Password Length (8-128)");
+    lengthisok = (length >= 8 && length <= 128)
+    if (!lengthisok) {
+    alert("Password must be between 8 and 128 characters - Please try again");
+   }
+ } 
+return length;
+}
+
+
+function passwordlowercase () {
+  var lowercase = confirm("Would you like to include lowercase letters? Press OK for yes, Cancel for no.");
+  return lowercase;
+}
+
+function passworduppercase () {
+  var uppercase = confirm("Would you like to include uppercase letters? Press OK for yes, Cancel for no.");
+  return uppercase;
+}
+
+function passwordnumeric () {
+  var numeric = confirm("Would you like to include numeric characters? Press OK for yes, Cancel for no.");
+  return numeric;
+}
+
+function passwordspecialcharacters () {
+  var specialcharacters = confirm("Would you like to include special characters? Press OK for yes, Cancel for no.");
+  return specialcharacters;
+}
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
