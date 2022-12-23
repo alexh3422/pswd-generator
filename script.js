4105d8cf90113355d1b78d0a73a2be1ca13134e3
Alex Hall <alexhallinthecloud@icloud.com>
2022-12-22 20:43:18 -0800

    updated js so that password ALWAYS includes at least 1 of each character type selected by user

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+ //function below allows the user to select how long their password will be. If they select anys number below 8 or above 128
+  //Then they will be prompted to try again 
+  function passwordlength () {
+    var lengthisok = false
+    while (!lengthisok) {
+      var length = prompt("Enter Password Length (8-128)");
+      lengthisok = (length >= 8 && length <= 128)
+      if (!lengthisok) {
+      alert("Password must be between 8 and 128 characters - Please try again");
+     }
+   } 
+  return length;
+  }
+  
+  // functions below take the user through the prompts selecting character types to create their password
+  function passwordlowercase () {
+    var lowercase = confirm("Must password include lowercase letters? Press OK for yes, Cancel for no.");
+    return lowercase;
+  }
+  
+  function passworduppercase () {
+    var uppercase = confirm("Must password include uppercase letters? Press OK for yes, Cancel for no.");
+    return uppercase;
+  }
+  
+  function passwordnumeric () {
+    var numeric = confirm("Must password include numeric characters? Press OK for yes, Cancel for no.");
+    return numeric;
+  }
+  
+  function passwordspecialcharacters () {
+    var specialcharacters = confirm("Must password include special characters? Press OK for yes, Cancel for no.");
+    return specialcharacters;
+  }
+  
+  
+  
 
 
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 function generatePassword () { 
   var charcount = passwordlength();
@@ -46,59 +74,59 @@ function generatePassword () {
   }
 
 // passworddomanin contains all allowed password characters
-
+  var passwordstring = ""
   var password = ""
   var i = 0
 
+  if (lowercase) {
+   passwordstring += lowercasecharacters[Math.floor(Math.random() * lowercasecharacters.length)]
+   i = i + 1
+  }
+  if (uppercase) {
+    passwordstring += uppercasecharacters[Math.floor(Math.random() * uppercasecharacters.length)]
+    i = i + 1
+  }
+  if (numeric) {
+    passwordstring += numericcharacters[Math.floor(Math.random() * numericcharacters.length)]
+   i = i + 1
+  }
+  if (special) {
+    passwordstring += specialcharacters[Math.floor(Math.random() * specialcharacters.length)]
+   i = i + 1
+  }
+  alert (passwordstring)
   while (i < charcount){
     
     // password = password.concat(c) // taking  those charactes out of  the created substrings and  adding them onto the password (final product)
-     password = password.concat(passworddomain[Math.floor(Math.random() * passworddomain.length)])
+     passwordstring = passwordstring.concat(passworddomain[Math.floor(Math.random() * passworddomain.length)])
     i = i + 1
  
   }
+
+  //password string contains all of  the characters selected by the user but now we need to randomize order so it doesnt always start with the same character types and is truly random
+  i = 0
+  alert (passwordstring)
+  while (passwordstring.length > 0) {
+    var randomindex = Math.floor(Math.random() * passwordstring.length)
+    var c = passwordstring[randomindex]
+    password = password.concat(c)
+    passwordstring = passwordstring.slice(0, randomindex).concat(passwordstring.slice(randomindex + 1))
+    alert (passwordstring + ":" + password)
+  }
+
   return password;
 }
 
-//function below allows the user to select how long their password will be. If they select anys number below 8 or above 128
-//Then they will be prompted to try again 
-function passwordlength () {
-  var lengthisok = false
-  while (!lengthisok) {
-    var length = prompt("Enter Password Length (8-128)");
-    lengthisok = (length >= 8 && length <= 128)
-    if (!lengthisok) {
-    alert("Password must be between 8 and 128 characters - Please try again");
-   }
- } 
-return length;
+ 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
 
-// functions below take the user through the prompts selecting character types to create their password
-function passwordlowercase () {
-  var lowercase = confirm("Must password include lowercase letters? Press OK for yes, Cancel for no.");
-  return lowercase;
-}
-
-function passworduppercase () {
-  var uppercase = confirm("Must password include uppercase letters? Press OK for yes, Cancel for no.");
-  return uppercase;
-}
-
-function passwordnumeric () {
-  var numeric = confirm("Must password include numeric characters? Press OK for yes, Cancel for no.");
-  return numeric;
-}
-
-function passwordspecialcharacters () {
-  var specialcharacters = confirm("Must password include special characters? Press OK for yes, Cancel for no.");
-  return specialcharacters;
-}
-
-
-
-
-
-
-// Add event listener to generate button
+// mainline code
+ // event listener for generate button
+var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
